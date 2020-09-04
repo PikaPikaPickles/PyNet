7d1d7ec1c0b5f4ef8e0f252ec07aff8002719474
PikaPikaPickles <61229514+PikaPikaPickles@users.noreply.github.com>
2020-09-04 14:45:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+count = int(input())
+i = 0
+a = list()
+while i < count:
+    a.append(int(input()))
+    i = i+1
+a.sort()
+i = 0
+while i< count:
+    if a[i]>0 or a[i] == 0:
+        print(a[i])
+    i = i+1
+i = 0
+while i< count:
+    a[i] = a[i] * (-1)
+    i = i + 1
+a.sort()
+i = 0
+while i< count:
+    if a[i]>0:
+        print(a[i]*-1)
+    i = i+1
+i = 0
